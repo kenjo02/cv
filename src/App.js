e0c5c83730911c2f7e81028c309b20eebb8d5ecf
kendall <kendallzamoramata@gmail.com>
2023-01-14 22:35:05 -0600

    curriculum

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div class="columnas">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div align="center">
+        
+       <h1>Kendall Johan Zamora Brenes</h1>
+       </div>
+       <div class="columnas1">
+       <h1>Direccion</h1>
+       <p>Alajuela:coyol</p>
+       <h1>identificacion</h1>
+       <p>1 18680615</p>
+       <p></p>
+       </div>
+       <div class="columnas2">
+       <h2>educacion</h2>
+        <p>Escuela San Lorenzo</p>
+        <p>2015</p>
+        <p>Certificado de conclusión de estudios del segundo ciclo de la educación general básica</p>
+        <h5>Colegio Jose Ramires Castro </h5>
+        <p>2022</p>
+        <p>Certificado de conclusión de estudios de Bachiller en Educacion media </p>
+        <h5>Colegio Jose Ramires Castro </h5>
+        <p>Tecnologia de la informacion y la comunicacion y desarrollo de las pequeñas aplicaciones de Software</p>
+        <h1>FORTALEZAS</h1>
+        <p>Capacidad de adaptación</p>
+        <p>Habilidades Comunicativas</p>
+        <p>Esfuerzo</p>
+        <p>Habilidades organizativas </p>
+        <p>Trabajo en equipo </p>
+        <p>Proactividad</p>
+        <p>Creatividad</p>
+        <h1>conocimientos tecnicos</h1>
+        <p>Conocimientos digitales (Excel, Word,Power Point) </p>
+        <p>desarrollo de pequeñas aplicacione de Software</p>
+        <p>ingles basico</p>
+        </div>
+
+        
+        
       </header>
     </div>
   );
