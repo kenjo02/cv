602fe7996c733d98eebe48c54ff0e9fbaf15cb98
kendall <kendallzamoramata@gmail.com>
2023-01-19 20:58:29 -0600

    cv

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import './App.css';
 
 function App() {
   return (
+    
     <div class="columnas">
       <header className="App-header">
-        <div align="center">
-        
+       
        <h1>Kendall Johan Zamora Brenes</h1>
-       </div>
+       <img src="https://scontent.fsjo5-1.fna.fbcdn.net/v/t39.30808-6/272119798_6863246043745737_1219492390352460150_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=4mCpFCawZiQAX-_b9MS&_nc_ht=scontent.fsjo5-1.fna&oh=00_AfBFwsJh7yejvwWoe3x1BR9T1gbHQFMpYYEqkgijHCQdqw&oe=63CE051D"height="300px" width="300px"/>
        <div class="columnas1">
        <h1>Direccion</h1>
        <p>Alajuela:coyol</p>
+       <p>urb:Monte rocoso</p>
        <h1>identificacion</h1>
        <p>1 18680615</p>
-       <p></p>
-       </div>
-       <div class="columnas2">
-       <h2>educacion</h2>
+       <h2>Telefono:</h2>
+        <a href="tel:+50684297121"><p color="black">84297121</p></a>
+       <h2>Correo:</h2>
+       <a href="mailto:kendallzamoramata@gmail.com"><p>kendallzamoramata@gmail.com</p></a>
+       
+       <p >.</p>
+      
+
+       <h2>Educacion</h2>
         <p>Escuela San Lorenzo</p>
         <p>2015</p>
         <p>Certificado de conclusión de estudios del segundo ciclo de la educación general básica</p>
-        <h5>Colegio Jose Ramires Castro </h5>
+        <h2>Colegio Jose Ramires Castro </h2>
         <p>2022</p>
         <p>Certificado de conclusión de estudios de Bachiller en Educacion media </p>
-        <h5>Colegio Jose Ramires Castro </h5>
+        <h2>Colegio Jose Ramires Castro </h2>
         <p>Tecnologia de la informacion y la comunicacion y desarrollo de las pequeñas aplicaciones de Software</p>
         <h1>FORTALEZAS</h1>
         <p>Capacidad de adaptación</p>
