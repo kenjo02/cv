a1b20711ea522aa49d5eec4bb208bc7280b3ad5c
kendall <kendallzamoramata@gmail.com>
2023-01-23 22:32:28 -0600

    Mayusculas

--- src/Datospersonales.js
+++ src/Datospersonales.js
@@ -2,8 +2,8 @@ const Datospersonales = () => {
     return <div>
        <h1>Direccion</h1>
        <p>Alajuela:coyol</p>
-       <p>urb:Monte rocoso</p>
-       <h1>identificacion</h1>
+       <p>Urb:Monte rocoso</p>
+       <h1>Identificacion</h1>
        <p>1 18680615</p>
        <h2>Telefono:</h2>
         <a href="tel:+50684297121"><p color="black">84297121</p></a>
--- src/Fortalezas.js
+++ src/Fortalezas.js
@@ -1,7 +1,7 @@
 const Fortalezas = () => {
 return <div>
     <p>Tecnologia de la informacion y la comunicacion y desarrollo de las pequeñas aplicaciones de Software</p>
-        <h1>FORTALEZAS</h1>
+        <h1>Fortalezas</h1>
         <p>Capacidad de adaptación</p>
         <p>Habilidades Comunicativas</p>
         <p>Esfuerzo</p>
@@ -9,10 +9,10 @@ return <div>
         <p>Trabajo en equipo </p>
         <p>Proactividad</p>
         <p>Creatividad</p>
-        <h1>conocimientos tecnicos</h1>
+        <h1>Conocimientos tecnicos</h1>
         <p>Conocimientos digitales (Excel, Word,Power Point) </p>
-        <p>desarrollo de pequeñas aplicacione de Software</p>
-        <p>ingles basico</p>
+        <p>Desarrollo de pequeñas aplicacione de Software</p>
+        <p>Ingles basico</p>
 </div>
 
 }
